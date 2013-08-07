040a4335d55516b35853cdafe8a0e88cd24c4308
Kevin F Browne <kbrowneatx@gmail.com>
2013-08-07 16:40:34 -0500

    created new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BattlePuzzles::Application.config.secret_key_base = 'f8788dd836fdb134990b739ff6d5b298c88c3a999e7246aa15a2eefa2933f0b984d6d833dc0c46482abdecc20fc00a286ef6496052dc33311627e964ef981985'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BattlePuzzles::Application.config.session_store :cookie_store, key: '_battle_puzzles_session'
